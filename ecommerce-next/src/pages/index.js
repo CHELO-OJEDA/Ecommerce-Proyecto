962256ee5ab180e973cdb6dd82d73773e0007468
CHELO-OJEDA <cheloojeda9@gmail.com>
2023-12-20 20:30:04 -0300

    Ecommerce

---
+++ ecommerce-next/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react';
+export default function () {
+  return (
+    <div>
+      <h2>Games Shop</h2>
+      <Button primary>Ir al login</Button>  
+    </div>
+  );
+}
